68e5b2daf8903dd61e0301bccca616793b479341
suresh.potey <sureshpotey@gmail.com>
2023-10-27 12:33:37 +0530

    my first script

---
+++ day1-intro/1st-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Authour: Potey Suresh
+#Description: the purpose is to check the 1st shell script commands
+#Date: 27-10-2023
+#last-modified: 27-10-2023
+
+ls
+date
+hostname
+git --version
